7ff3dff0c1f016295885e812ae003334c3202bf5
fjunya <jfjfje@gmail.com>
2019-09-08 12:37:46 +0900

    settings.pyのタイムゾーンの変更

--- project/project/settings.py
+++ project/project/settings.py
@@ -72,8 +72,8 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
+LANGUAGE_CODE = 'ja'
+TIME_ZONE = 'Asia/Tokyo'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
