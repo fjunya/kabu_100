d653a8453c9cd50a706d6597cb09a4298ac0ec26
fjunya <jfjfje@gmail.com>
2019-09-08 19:00:29 +0900

    RawPricesを追加

--- project/app/models.py
+++ project/app/models.py
@@ -3,3 +3,13 @@ from django.db import models
 class Company(models.Model):
     code = models.IntegerField("銘柄コード", primary_key=True)
     name = models.CharField("会社名", max_length=200)
+
+class RawPrices(models.Model):
+    code = models.IntegerField("銘柄コード")
+    date = models.DateField("日付")
+    open_price = models.IntegerField("始値")
+    close_price = models.IntegerField("終値")
+    high_price = models.IntegerField("高値")
+    low_price = models.IntegerField("安値")
+    volume = models.IntegerField("出来高")
+    adjustment_close_price = models.IntegerField("調整後終値")
