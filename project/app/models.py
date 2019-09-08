b2b99dd9c726ae4bf47887b168b80b121fbfeb30
fjunya <jfjfje@gmail.com>
2019-09-08 14:32:02 +0900

    DBの設定を追加

--- project/app/models.py
+++ project/app/models.py
@@ -1,2 +1,5 @@
 from django.db import models
 
+class Company(models.Model):
+    code = models.IntegerField("銘柄コード", primary_key=True)
+    name = models.CharField("会社名", max_length=200)
--- project/project/settings.py
+++ project/project/settings.py
@@ -51,10 +51,14 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'project.wsgi.application'
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default':{
+         'ENGINE': 'django.db.backends.mysql',
+         'NAME': 'kabu_100',
+         'USER': '',
+         'PASSWORD': '',
+         'HOST': '127.0.0.1',
+         'PORT': 3306,
+    },
 }
 
 AUTH_PASSWORD_VALIDATORS = [
