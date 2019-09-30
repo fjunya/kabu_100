f3b5fc462a02ceb8cbed767e0109bb639376e166
fjunya <jfjfje@gmail.com>
2019-09-30 10:08:08 +0900

    adminサイトの設定を追加

--- project/app/models.py
+++ project/app/models.py
@@ -4,6 +4,9 @@ class Company(models.Model):
     code = models.IntegerField("銘柄コード", primary_key=True)
     name = models.CharField("会社名", max_length=200)
 
+    def __str__(self):
+        return "{code} : {name}".format(code=self.code, name=self.name)
+
 class RawPrices(models.Model):
     code = models.IntegerField("銘柄コード")
     date = models.DateField("日付")
@@ -13,6 +16,9 @@ class RawPrices(models.Model):
     low_price = models.IntegerField("安値")
     volume = models.IntegerField("出来高")
     adjustment_close_price = models.IntegerField("調整後終値")
+
+    def __str__(self):
+        return "{code} : {date}".format(code=self.code, date=self.date)
 
     class Meta:
         constraints = [
