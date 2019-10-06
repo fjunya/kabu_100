a8a1c572868954cf762a42e42870c76e33705913
fjunya <jfjfje@gmail.com>
2019-10-06 10:16:00 +0900

    移動平均値を求める処理で使用するデータを終値から調整後終値に変更

--- project/app/management/commands/view_stock_chart.py
+++ project/app/management/commands/view_stock_chart.py
@@ -22,18 +22,18 @@ class Command(BaseCommand):
         rolling_day = 75
         start_date = datetime.datetime.strptime(start_date_str + "-01", "%Y-%m-%d")
         # 75日移動平均線を求める為に余分にデータを取得。土日を考慮して110日より前から取得
-        rawPrices = RawPrices.objects\
+        raw_prices = RawPrices.objects\
             .filter(code=code)\
             .filter(date__gt=start_date - datetime.timedelta(days=110))\
             .order_by('date')
-        rawPriceList = list(map(lambda x: [x.date, x.open_price, x.close_price], rawPrices))
-        columns = ["date", "open_price", "close_price"]
-        df = pd.DataFrame(rawPriceList, columns=columns)
+        raw_price_list = list(map(lambda x: [x.date, x.adjustment_close_price], raw_prices))
+        columns = ["date", "adjustment_close_price"]
+        df = pd.DataFrame(raw_price_list, columns=columns)
         df.date = pd.to_datetime(df.date)
         df.set_index("date", inplace=True)
-        df.loc[start_date_str:, ['open_price', 'close_price']].plot()
+        df.loc[start_date_str:, ['adjustment_close_price']].plot()
         # 移動平均線を求める為に、全てのデータから計算(表示する期間だけで計算すると最初の方の移動平均線を求められない為)
-        roll_mean = df.close_price.rolling(window=rolling_day).mean()
+        roll_mean = df.adjustment_close_price.rolling(window=rolling_day).mean()
         # 移動平均線を表示
         roll_mean.loc[start_date_str:].plot()
         plt.xlabel('Date')
